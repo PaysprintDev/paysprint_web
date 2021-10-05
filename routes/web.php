1b54aa83b3896f086bbc1cf06370fa7bcb5e6c85
Adebambo Adenuga <adenugaadebambo411@gmail.com>
2021-10-05 14:10:57 +0100

    update web route

--- routes/web.php
+++ routes/web.php
@@ -84,10 +84,10 @@ Route::get('/run-queue', function () {
 Route::get('/clear', function () {
 
 	Artisan::call('config:clear');
-	Artisan::call('config:cache');
-	Artisan::call('cache:clear');
-	Artisan::call('route:clear');
-	Artisan::call('view:clear');
+	// Artisan::call('config:cache');
+	// Artisan::call('cache:clear');
+	// Artisan::call('route:clear');
+	// Artisan::call('view:clear');
 	return "Package Cleared!";
 });
 
