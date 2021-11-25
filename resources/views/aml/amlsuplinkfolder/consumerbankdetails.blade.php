599b336d3ffa7b31f87532a4ec32632ee56408c6
Adebambo Adenuga <adenugaadebambo411@gmail.com>
2021-11-25 13:38:43 +0100

    aml update

---
+++ resources/views/aml/amlsuplinkfolder/consumerbankdetails.blade.php
@@ -0,0 +1,111 @@
+@extends('layouts.dashboard')
+
+@section('dashContent')
+
+
+<?php use \App\Http\Controllers\User; ?>
+<?php use \App\Http\Controllers\BankWithdrawal; ?>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+         Bank Details
+      </h1>
+      <ol class="breadcrumb">
+      <li><a href="{{ route('Admin') }}"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+        <li class="active">Bank Details</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="box">
+            <div class="box-header">
+              <div class="row">
+                <div class="col-md-2 col-md-offset-0">
+                <button class="btn btn-secondary btn-block bg-red" onclick="goBack()"><i class="fas fa-chevron-left"></i> Go back</button>
+            </div>
+            </div>
+
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body table table-responsive">
+
+              <table class="table table-bordered table-striped" id="example3">
+
+                <thead>
+                  <div class="row">
+                    <div class="col-md-6">
+                      <h3 id="period_start"></h3>
+                    </div>
+                    <div class="col-md-6">
+                      <h3 id="period_stop"></h3>
+                    </div>
+                  </div>
+                <tr>
+                  <th>S/N</th>
+                  <th>Business Name</th>
+                  <th>Name</th>
+                  <th>Bank Name</th>
+                  <th>Transit No.</th>
+                  <th>Branch Code</th>
+                  <th>Account Name</th>
+                  <th>Account Number</th>
+                  <th>Date Created</th>
+                </tr>
+                </thead>
+                <tbody>
+                    @if (count($data['bankdetails']) > 0)
+                    <?php $i = 1;?>
+                        @foreach ($data['bankdetails'] as $data)
+
+
+                        @if($user = \App\User::where('id', $data->user_id)->first())
+
+
+                            @if ($user->accountType == Request::get('users'))
+
+                                <tr>
+                                    <td>{{ $i++ }}</td>
+
+                                    <td>{{ $user->businessname }}</td>
+                                    <td>{{ $user->name }}</td>
+                                    <td>{{ $data->bankName }}</td>
+                                    <td>{{ $data->transitNumber }}</td>
+                                    <td>{{ $data->branchCode }}</td>
+                                    <td>{{ $data->accountName }}</td>
+                                    <td>{{ $data->accountNumber }}</td>
+                                    <td>{{ date('d/m/Y', strtotime($data->created_at)) }}</td>
+
+                                </tr>
+                            @endif
+
+                        @endif
+                        @endforeach
+
+                    @else
+                    <tr>
+                        <td colspan="9" align="center">No record available</td>
+                    </tr>
+                    @endif
+                </tbody>
+              </table>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+@endsection
+
+
---
+++ resources/views/aml/amlsuplinkfolder/pendingtransferaml.blade.php
@@ -0,0 +1,114 @@
+@extends('layouts.dashboard')
+
+@section('dashContent')
+
+
+<?php use \App\Http\Controllers\User; ?>
+<?php use \App\Http\Controllers\AnonUsers; ?>
+<?php use \App\Http\Controllers\Statement; ?>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+         Pending Transfer
+      </h1>
+      <ol class="breadcrumb">
+      <li><a href="{{ route('Admin') }}"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+        <li class="active">Pending Transfer</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+        <br>
+          <button class="btn btn-secondary btn-block bg-red" onclick="goBack()"><i class="fas fa-chevron-left"></i> Go back</button>
+        <br>
+
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="box">
+
+            <!-- /.box-header -->
+            <div class="box-body table table-responsive">
+
+              <table class="table table-bordered table-striped" id="example3">
+
+                <thead>
+                  <div class="row">
+                    <div class="col-md-6">
+                      <h3 id="period_start"></h3>
+                    </div>
+                    <div class="col-md-6">
+                      <h3 id="period_stop"></h3>
+                    </div>
+                  </div>
+                <tr>
+                  <th>S/N</th>
+                  <th>Country</th>
+                  <th>Total Amount</th>
+                  <th>Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                    @if (count($data['pendingtransfer']) > 0)
+                    <?php $i = 1;?>
+                        @foreach ($data['pendingtransfer'] as $data)
+                        <tr>
+                            <td>{{ $i++ }}</td>
+
+                            <td>{{ $data->country }}</td>
+
+                            @if($user = \App\User::where('country', $data->country)->first())
+
+                            @php
+                                $currencyCode = $user->currencyCode;
+                                $currencySymbol = $user->currencySymbol;
+                            @endphp
+
+                            @else
+
+
+                            @php
+                                $currencyCode = "";
+                                $currencySymbol = "";
+                            @endphp
+
+                            @endif
+
+                            @if($amount = \App\AnonUsers::where('country', $data->country)->sum('wallet_balance'))
+                                <td style="font-weight: 700;">{{ $currencySymbol.' '.number_format($amount, 2) }}</td>
+                            @endif
+
+
+                            <td>
+                                <a type="button" href="{{ route('texts to transfer by country', 'country='.$data->country) }}" class="btn btn-primary btn-block">View details</a>
+                            </td>
+
+                        </tr>
+                        @endforeach
+
+                    @else
+                    <tr>
+                        <td colspan="4" align="center">No record available</td>
+                    </tr>
+                    @endif
+                </tbody>
+              </table>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+@endsection
+
+
