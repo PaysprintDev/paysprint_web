f080502e669d30035c178f48826e5304641ea32e
Adebambo Adenuga <adenugaadebambo41@gmail.com>
2022-05-25 11:25:18 +0100

    new changes

--- resources/views/merchant/pages/shop/orders.blade.php
+++ resources/views/merchant/pages/shop/orders.blade.php
@@ -27,7 +27,7 @@
                         <div class="about-l">
                             <ul class="about-link">
                                 <li class="go-home"><a href="{{ url()->previous() }}">Home</a></li>
-                                <li class="about-p"><span>Your checkout</span></li>
+                                <li class="about-p"><span>Your orders</span></li>
                             </ul>
                         </div>
                     </div>
@@ -44,6 +44,7 @@
                 <h1>Orders</h1>
             </div>
         </div>
+
 
         <!-- displaying the ordders -->
         @if (count($data['orders']) > 0)
@@ -108,10 +109,68 @@
                                 DETAILS</a>
                         </div>
                     </div>
+                @else
+                    <div class="row mt-3 mb-5">
+                        <div class="col-md-3">
+                            <p><img style="width:100%; border-radius: 10px;"
+                                    src="https://www.removalmedia.com/wp-content/uploads/2020/08/internet-content-removal-expert.jpg">
+                            </p>
+                        </div>
+                        <div class="col-md-6">
+
+                            <table class="table table-striped table-responsive">
+
+                                <tbody>
+                                    <tr>
+                                        <td>Order Id:</td>
+                                        <td>
+                                            <p>{!! $value->orderId !!}</p>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="2">
+                                            <p>Out of stock</p>
+                                        </td>
+                                    </tr>
+
+                                    <tr>
+                                        <td>Quantity:</td>
+                                        <td>
+                                            <p>{!! $value->quantity !!}</p>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Payment Status:</td>
+                                        <td>
+                                            <p><span
+                                                    class="{{ $value->paymentStatus == 'not paid' ? 'badge bg-danger' : 'badge bg-success' }}">{{ $value->paymentStatus == 'not paid' ? 'not paid' : 'Paid' }}</span>
+                                            </p>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Delivery Status:</td>
+                                        <td>
+                                            <p>
+                                                <span
+                                                    class="{{ $value->deliveryStatus == 'off' ? 'badge bg-danger' : 'badge bg-success' }}">{{ $value->deliveryStatus == 'off' ? 'Not Delivered' : 'Delivered' }}</span>
+                                            </p>
+                                        </td>
+                                    </tr>
+                                </tbody>
+
+                            </table>
+
+
+                        </div>
+                        <div class="col-md-3">
+                            <a class="text-center" href="#"
+                                style="color: red; font-weight:lighter; font-size:18px;">Removed from store</a>
+                        </div>
+                    </div>
                 @endif
             @endforeach
-            @else
-                    <div class="col-md-12 text-center mt-3 mb-4" style="font-size: 20px; font-weight:bold">No orders</div>
+        @else
+            <div class="col-md-12 text-center mt-3 mb-4" style="font-size: 20px; font-weight:bold">No orders</div>
         @endif
     </section>
 
