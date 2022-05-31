de40de5b2aac648c3647f0c65bc3a6fa0a2862e3
Abimbola <youngskima@gmail.com>
2022-05-31 12:34:21 +0100

    updated

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1842,7 +1842,7 @@ class AdminController extends Controller
     }
 
     public function editInvestorPosts(Request $req, $id){
-
+                dd($req->all());
         $getPost = CreatePost::where('id', $id)->first();
 
         $docPath = $getPost->investment_document;
