8a0873c96dc03851ae3834e6cd46bfee8ece6cbd
Abimbola <youngskima@gmail.com>
2022-06-01 17:42:26 +0100

    updated

--- resources/views/include/adminbottom.blade.php
+++ resources/views/include/adminbottom.blade.php
@@ -30,6 +30,8 @@ crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     $.widget.bridge('uibutton', $.ui.button);
 </script>
 
+<!-- bootstrap js-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <!-- Bootstrap 3.3.7 -->
 <script src="{{ asset('ext/bower_components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
 
