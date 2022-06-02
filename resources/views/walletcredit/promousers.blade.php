df363fe64b9ecc46fbbf581ca61e5b85bc14eaa1
Abimbola <youngskima@gmail.com>
2022-06-02 13:53:33 +0100

    updated

--- resources/views/walletcredit/promousers.blade.php
+++ resources/views/walletcredit/promousers.blade.php
@@ -58,31 +58,56 @@
                             <td>{{ $counter++}}</td>
                             <td>{{ $promousers->date}}</td>
                             <td>{{ $promousers->email}}</td>
-                            <td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#staticBackdrop">Top-up</button></td>
+                            <td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#staticBackdrop{{ $promousers->id }}">Top-up</button></td>
                         </tr>
+
+                        <!-- Modal -->
+                  <div class="modal fade" id="staticBackdrop{{ $promousers->id }}" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h5 class="modal-title" id="staticBackdropLabel">Top-up Credit</h5>
+                        </div>
+                        <div class="modal-body">
+                          <form action="{{route('top up')}}" method="post">
+                            @csrf
+                            <div class="row"> 
+                          @if($user= \App\User::where('email',$promousers->email)->first())
+                              <div class="col-md-6 form-group">
+                                <label>Name of User</label>
+                                <p>{{ $user->name}}</p>
+                              </div>
+                              <div class="col-md-6 form-group">
+                                <label class="text-danger">Country: {{ $user->country}}</label> <br>
+                                <label class="text-danger">Currency: {{ $user->currencyCode}}</label>
+                              </div>
+                              <div class="col-md-12 form-group">
+                                <label>Wallet Balance: {{ $user->wallet_balance}}</label>
+                              </div>
+                            
+                              <div class="col-md-12 form-group">
+                                <label>Top-up Credit</label>
+                                <input type="text" name="topup_credit" class="form-control"> 
+                              </div>
+                              <input type="hidden" name="userid" value="{{ $user->id}}">
+                          </div>
+                          @endif
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                          <button type="submit" class="btn btn-primary">Top-up</button>
+                        </div>
+                      </form>
+                      </div>
+                    </div>
+                  </div>
+
                     @endforeach
                     @endif
                     <!-- modal -->
                     <!-- Button trigger modal -->
 
-                  <!-- Modal -->
-                  <div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                      <div class="modal-content">
-                        <div class="modal-header">
-                          <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
-                          <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                          ...
-                        </div>
-                        <div class="modal-footer">
-                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                          <button type="button" class="btn btn-primary">Understood</button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+                  
   
                     </tbody>
                 </table>
