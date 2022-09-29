aa98640915866e26c9669e8dbfdf4816f76e3be7
Adebambo Adenuga <35952059+Pils36@users.noreply.github.com>
2022-09-29 18:10:19 +0100

    moex finalizing��

--- app/Traits/Moex.php
+++ app/Traits/Moex.php
@@ -155,14 +155,33 @@ trait Moex
     public function addTransactionToMoex($data)
     {
 
-        $data['paymentBranchId'] = env('APP_ENV') === 'local' ? '8349-0001' : '0001-0001';
-        $data['receiverCountry'] = env('APP_ENV') === 'local' ? 'ESP' : $data['receiverCountry'];
-        $data['senderCountry'] = env('APP_ENV') === 'local' ? 'ESP' : $data['senderCountry'];
-        $data['originCountry'] = env('APP_ENV') === 'local' ? 'ESP' : $data['originCountry'];
-        $data['currencyToPay'] = env('APP_ENV') === 'local' ? 'EUR' : $data['currencyToPay'];
-        $data['currencySent'] = env('APP_ENV') === 'local' ? 'EUR' : $data['currencySent'];
 
-        dd($data);
+
+        // $data['receiverCountry'] = env('APP_ENV') === 'local' ? 'ESP' : $data['receiverCountry'];
+        // $data['senderCountry'] = env('APP_ENV') === 'local' ? 'ESP' : $data['senderCountry'];
+        // $data['originCountry'] = env('APP_ENV') === 'local' ? 'ESP' : $data['originCountry'];
+        // $data['currencyToPay'] = env('APP_ENV') === 'local' ? 'EUR' : $data['currencyToPay'];
+        // $data['currencySent'] = env('APP_ENV') === 'local' ? 'EUR' : $data['currencySent'];
+
+        $getBranchId = $this->availableBranchList($data['receiverCountry']);
+
+        if($getBranchId['branchId'] === '-12'){
+
+            $responseData = [
+                'error' => "Sorry payout is not available to this country at the moment."
+            ];
+
+            return $responseData;
+        }
+        else{
+            $data['paymentBranchId'] = $getBranchId['branchId'];
+        }
+
+
+
+
+
+
 
 
 
@@ -221,7 +240,7 @@ trait Moex
                     <PaymentBranchAuxId xsi:type="xsd:string">?</PaymentBranchAuxId>
                     <OriginCountry xsi:type="xsd:string">' . $data['originCountry'] . '</OriginCountry>
                     <Reference xsi:type="xsd:string">?</Reference>
-                    <AuxiliaryInfo xsi:type="xsd:string">?</AuxiliaryInfo>
+                    <AuxiliaryInfo xsi:type="xsd:string">'.json_encode($data['auxiliaryInfo']).'</AuxiliaryInfo>
                         </transaction>
                     </urn:MEAddTransaction>
                 </soapenv:Body>
@@ -230,19 +249,102 @@ trait Moex
 
         $result = $this->moExPostCurl();
 
-
-
         $doc->loadXML($result);
 
-        $TransactionId = $doc->getElementsByTagName("TransactionId");
+        $TransactionId     = $doc->getElementsByTagName("TransactionId");
+        $TransactionDate     = $doc->getElementsByTagName("TransactionDate");
+        $SenderId     = $doc->getElementsByTagName("SenderId");
+        $Sender     = $doc->getElementsByTagName("Sender");
+        $SenderAddress     = $doc->getElementsByTagName("SenderAddress");
+        $SenderCity     = $doc->getElementsByTagName("SenderCity");
+        $SenderCountry     = $doc->getElementsByTagName("SenderCountry");
+        $SenderIdDocumentNumber     = $doc->getElementsByTagName("SenderIdDocumentNumber");
+        $SenderIdDocumentType     = $doc->getElementsByTagName("SenderIdDocumentType");
+        $ReceiverId     = $doc->getElementsByTagName("ReceiverId");
+        $Receiver     = $doc->getElementsByTagName("Receiver");
+        $ReceiverAddress     = $doc->getElementsByTagName("ReceiverAddress");
+        $ReceiverCity     = $doc->getElementsByTagName("ReceiverCity");
+        $ReceiverCountry     = $doc->getElementsByTagName("ReceiverCountry");
+        $BankDeposit     = $doc->getElementsByTagName("BankDeposit");
+        $BankName     = $doc->getElementsByTagName("BankName");
+        $BankAddress     = $doc->getElementsByTagName("BankAddress");
+        $BankAccount     = $doc->getElementsByTagName("BankAccount");
+        $AmountToPay     = $doc->getElementsByTagName("AmountToPay");
+        $CurrencyToPay     = $doc->getElementsByTagName("CurrencyToPay");
+        $AmountSent     = $doc->getElementsByTagName("AmountSent");
+        $CurrencySent     = $doc->getElementsByTagName("CurrencySent");
+        $SenderMessage     = $doc->getElementsByTagName("SenderMessage");
+        $PaymentBranchId     = $doc->getElementsByTagName("PaymentBranchId");
+        $PaymentBranchName     = $doc->getElementsByTagName("PaymentBranchName");
+        $PaymentBranchAddress     = $doc->getElementsByTagName("PaymentBranchAddress");
+        $PaymentBranchAuxId     = $doc->getElementsByTagName("PaymentBranchAuxId");
+        $OriginCountry     = $doc->getElementsByTagName("OriginCountry");
+        $TransactionStatus     = $doc->getElementsByTagName("TransactionStatus");
+
         $Description = $doc->getElementsByTagName("Description");
 
         if ($TransactionId->length > 0) {
             $transactionId = $TransactionId->item(0)->nodeValue;
+            $transactionDate = $TransactionDate->item(0)->nodeValue;
+            $senderId = $SenderId->item(0)->nodeValue;
+            $sender = $Sender->item(0)->nodeValue;
+            $senderAddress = $SenderAddress->item(0)->nodeValue;
+            $senderCity = $SenderCity->item(0)->nodeValue;
+            $senderCountry = $SenderCountry->item(0)->nodeValue;
+            $senderIdDocumentNumber = $SenderIdDocumentNumber->item(0)->nodeValue;
+            $senderIdDocumentType = $SenderIdDocumentType->item(0)->nodeValue;
+            $receiverId = $ReceiverId->item(0)->nodeValue;
+            $receiver = $Receiver->item(0)->nodeValue;
+            $receiverAddress = $ReceiverAddress->item(0)->nodeValue;
+            $receiverCity = $ReceiverCity->item(0)->nodeValue;
+            $receiverCountry = $ReceiverCountry->item(0)->nodeValue;
+            $bankDeposit = $BankDeposit->item(0)->nodeValue;
+            $bankName = $BankName->item(0)->nodeValue;
+            $bankAddress = $BankAddress->item(0)->nodeValue;
+            $bankAccount = $BankAccount->item(0)->nodeValue;
+            $amountToPay = $AmountToPay->item(0)->nodeValue;
+            $currencyToPay = $CurrencyToPay->item(0)->nodeValue;
+            $amountSent = $AmountSent->item(0)->nodeValue;
+            $currencySent = $CurrencySent->item(0)->nodeValue;
+            $senderMessage = $SenderMessage->item(0)->nodeValue;
+            $paymentBranchId = $PaymentBranchId->item(0)->nodeValue;
+            $paymentBranchName = $PaymentBranchName->item(0)->nodeValue;
+            $paymentBranchAddress = $PaymentBranchAddress->item(0)->nodeValue;
+            $paymentBranchAuxId = $PaymentBranchAuxId->item(0)->nodeValue;
+            $originCountry = $OriginCountry->item(0)->nodeValue;
+            $transactionStatus = $TransactionStatus->item(0)->nodeValue;
             $description = $Description->item(0)->nodeValue;
 
             $responseData = [
                 'transactionId' => $transactionId,
+                'transactionDate' => $transactionDate,
+                'senderId' => $senderId,
+                'sender' => $sender,
+                'senderAddress' => $senderAddress,
+                'senderCity' => $senderCity,
+                'senderCountry' => $senderCountry,
+                'senderIdDocumentNumber' => $senderIdDocumentNumber,
+                'senderIdDocumentType' => $senderIdDocumentType,
+                'receiverId' => $receiverId,
+                'receiver' => $receiver,
+                'receiverAddress' => $receiverAddress,
+                'receiverCity' => $receiverCity,
+                'receiverCountry' => $receiverCountry,
+                'bankDeposit' => $bankDeposit,
+                'bankName' => $bankName,
+                'bankAddress' => $bankAddress,
+                'bankAccount' => $bankAccount,
+                'amountToPay' => $amountToPay,
+                'currencyToPay' => $currencyToPay,
+                'amountSent' => $amountSent,
+                'currencySent' => $currencySent,
+                'senderMessage' => $senderMessage,
+                'paymentBranchId' => $paymentBranchId,
+                'paymentBranchName' => $paymentBranchName,
+                'paymentBranchAddress' => $paymentBranchAddress,
+                'paymentBranchAuxId' => $paymentBranchAuxId,
+                'originCountry' => $originCountry,
+                'transactionStatus' => $transactionStatus,
                 'description' => $description
             ];
         }
@@ -254,7 +356,51 @@ trait Moex
         }
 
 
+
         return $responseData;
+    }
+
+
+    public function availableBranchList($country)
+    {
+        $doc = new \DOMDocument('1.0', 'utf-8');
+        $responseData = [];
+
+        $this->moexPost = '<soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+                xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
+                xmlns:urn="urn:WSmoneyIntf-IWSmoney">
+                <soapenv:Header/>
+                <soapenv:Body>
+                <urn:MEGetActiveExtBranchesMoEx
+                soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+                <login xsi:type="urn:TWSauth" xmlns:urn="urn:WSmoneyClasses">
+                    <Username xsi:type="xsd:string">' . config("constants.moex.username") . '</Username>
+                    <Password xsi:type="xsd:string">' . config("constants.moex.password") . '</Password>
+                    <Version xsi:type="xsd:string">' . config("constants.moex.version") . '</Version>
+                </login>
+                <IdCountry xsi:type="xsd:string">'.$country.'</IdCountry>
+                </urn:MEGetActiveExtBranchesMoEx>
+                </soapenv:Body>
+                </soapenv:Envelope>';
+
+        $result = $this->moExPostCurl();
+
+        $doc->loadXML($result);
+
+        $Id = $doc->getElementsByTagName("Id");
+
+        if ($Id->length > 0) {
+            $Id = $Id->item(0)->nodeValue;
+
+            $responseData = [
+                'branchId' => $Id
+            ];
+        }
+
+        return $responseData;
+
+
     }
 
 
